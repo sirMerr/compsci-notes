455bc30cabadacb46063c973bf549c7411551e58
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2017-09-28 01:27:47 -0400

    :wrench: clean up workspace

--- development/php/lab3.php
+++ development/php/lab3.php
@@ -23,21 +23,39 @@ $country = "";
 $handle = fopen ( 'countries.txt' , 'r' );
 
 // Select the first line (a country)
-$dateEvent = fgets($handle, 4096);
+$countries = array();
 
+while (!feof($handle)) {
+    // Assign next line
+    $newTask = fgets($handle, 4096);
+
+    // Add to array
+    array_push($countries, substr($newTask, 0, count($newTask) - 2));
+}
 
 // Display
-echo "<form>"."</br>";
+echo "<form id='userform' action='form.php' method='post'>"."</br>";
 echo "Your name:<br>";
-echo "<input type='text' name='name'>"."<br>";
+echo "<input type='text' name='name'>"."<br><br>";
+
 echo "<input type='radio' name='gender' value='male' checked> Male<br>";
-echo "<input type='radio' name='gender' value='female'> Female<br>";
+echo "<input type='radio' name='gender' value='female'> Female<br><br>";
+
 echo "<input type=\"checkbox\" name=\"poker\" value=\"Poker\">Poker<br>";
 echo "<input type=\"checkbox\" name=\"biking\" value=\"Biking\">Biking<br>";
 echo "<input type=\"checkbox\" name=\"dancing\" value=\"Dancing\">Dancing<br>";
 echo "<input type=\"checkbox\" name=\"programming\" value=\"Programming\">Programming<br>";
 echo "<input type=\"checkbox\" name=\"coup\" value=\"Coup\">Coup<br>";
 echo "<input type=\"checkbox\" name=\"nothing\" value=\"Nothing\">Nothing<br>";
-echo "<input type=\"checkbox\" name=\"sleep\" value=\"Sleep\">Sleep<br>";
+echo "<input type=\"checkbox\" name=\"sleep\" value=\"Sleep\">Sleep<br><br>";
+
+echo "<select name='countrylist' form='userform'>"."<br>";
+foreach ($countries as &$value) {
+    echo "<option value=$value>$value</option>";
+}
+echo "<option value=\"volvo\">Volvo</option><br>";
+echo "<option value=\"volvo\">Volvo</option><br>";
+echo "</select>"."<br>";
 echo "<input type='submit' value='Submit'>"."<br>";
+
 echo "</form>"."</br>";
